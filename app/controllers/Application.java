04db939a2e9b451a4b6f47db6612ec763691da93
ArkeologeN <hamza@plumtreegroup.net>
2012-08-30 20:40:22 +0500

    Database Integration with Task Model!

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,34 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.util.List;
 
+import models.Task;
+import play.mvc.*;
 import views.html.*;
+import play.db.ebean.Transactional;
 
 public class Application extends Controller {
   
   public static Result index() {
     return ok(index.render("Your new application is ready."));
+  }
+  
+  @Transactional
+  public static Result join() {
+	  /*
+	  Task task = new Task();
+	  task.name = "Goto Office";
+	  task.dueDate = new Date();
+	  task.done = false;
+	  try {
+		  task.save();
+	  } catch(Exception ex) {
+		  System.out.println(ex.getMessage());
+	  }
+	  */
+	  List<Task> tasks = Task.find.all();
+	  
+	  return ok(index.render("Total Task: "+tasks.size()));
   }
   
 }
---
+++ app/models/Task.java
@@ -0,0 +1,30 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity 
+@Table(name="tasks")
+public class Task extends Model {
+
+  @Id
+  @Constraints.Min(10)
+  public Long id;
+  
+  @Constraints.Required
+  public String name;
+  
+  public boolean done;
+  
+  @Formats.DateTime(pattern="dd/MM/yyyy")
+  public Date dueDate = new Date();
+  
+  public static Finder<Long,Task> find = new Finder<Long,Task>(
+    Long.class, Task.class
+  ); 
+
+}
